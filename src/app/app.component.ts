a8289c7f7b89b0de5ef33dd64d75484cb384593d
contactjorge <contactjorge@thejorgelopez.com>
2018-01-29 19:53:56 -0600

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,94 @@
+/**
+ * Angular 2 decorators and services
+ */
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { environment } from 'environments/environment';
+import { AppState } from './app.service';
+import { HttpClient } from '@angular/common/http';
+
+/**
+ * App Component
+ * Top Level Component
+ */
+@Component({
+  selector: 'app',
+  encapsulation: ViewEncapsulation.None,
+  styleUrls: [
+    './app.component.css'
+  ],
+  template: `
+    <nav>
+      <a [routerLink]=" ['./'] "
+        routerLinkActive="active" [routerLinkActiveOptions]= "{exact: true}">
+        Index
+      </a>
+      <a [routerLink]=" ['./home'] "
+        routerLinkActive="active" [routerLinkActiveOptions]= "{exact: true}">
+        Home
+      </a>
+      <a [routerLink]=" ['./detail'] "
+        routerLinkActive="active" [routerLinkActiveOptions]= "{exact: true}">
+        Detail
+      </a>
+      <a [routerLink]=" ['./barrel'] "
+        routerLinkActive="active" [routerLinkActiveOptions]= "{exact: true}">
+        Barrel
+      </a>
+      <a [routerLink]=" ['./about'] "
+        routerLinkActive="active" [routerLinkActiveOptions]= "{exact: true}">
+        About
+      </a>
+      <a *ngIf="showDevModule" [routerLink]=" ['./dev-module'] "
+         routerLinkActive="active" [routerLinkActiveOptions]= "{exact: true}">
+        DevModule
+      </a>
+    </nav>
+
+    <main>
+      <router-outlet></router-outlet>
+    </main>
+
+    <pre class="app-state">this.appState.state = {{ appState.state | json }}</pre>
+
+    <footer>
+      <span>Angular Starter by <a [href]="twitter">@gdi2290</a></span>
+      <div>
+        <a [href]="url">
+          <img [src]="tipe" width="25%">
+        </a>
+      </div>
+      
+      <div id="dataUrl">
+        {{data}}
+      </div>
+    </footer>
+  `
+})
+export class AppComponent implements OnInit {
+  public name = 'Angular Starter';
+  public tipe = 'assets/img/tipe.png';
+  public twitter = 'https://twitter.com/gdi2290';
+  public url = 'https://tipe.io';
+  public showDevModule: boolean = environment.showDevModule;
+
+  constructor(
+    public appState: AppState,
+    private http: HttpClient
+  ) {console.log('This http is fine ', this.http)}
+
+  public ngOnInit() {
+
+    let firstUrl = 'https://api.mlab.com/api/1/databases/wondersign/collections/products?apiKey=B_o7qfV7j4diajcASjmKeB4sBzaBs76e';
+    this.http.get(firstUrl).subscribe((data) => {console.log('Getting Data ', data); });
+    console.log('Initial App State', this.appState.state);
+  }
+
+}
+
+/**
+ * Please review the https://github.com/AngularClass/angular2-examples/ repo for
+ * more angular app examples that you may copy/paste
+ * (The examples may not be updated as quickly. Please open an issue on github for us to update it)
+ * For help or questions please contact us at @AngularClass on twitter
+ * or our chat on Slack at https://AngularClass.com/slack-join
+ */
